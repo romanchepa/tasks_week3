559af29fe2af6dbf2e4a8a32f5d2cd774a8201c6
Roman Chepa <romanchepa@mail.ru>
2020-12-05 20:50:34 +0700

    Изменение

--- referat.py
+++ referat.py
@@ -1,25 +1,17 @@
-with open('referat.txt', 'r', encoding = 'utf-8')as f: # Читаем файл и считаем всю строку.
-  content = f.read()
-  print(len(content))
-
-
-with open('referat.txt') as f: # Считаем количество слов в тексте
-  lines = f.readlines()
-
-
-with open('referat.txt', 'r') as f: # Считаем количество слов в тексте
+with open('referat.txt', 'r', encoding = 'utf-8')as referat_file:
+  with open( 'referat2.txt', 'w') as new_referat_file:
+    for line in referat_file:
+      w = line.replace('.', '!')
+      new_referat_file.write(w)
+    referat_file.seek(0)
+    content = referat_file.read()
+    print(len(content))
+    referat_file.seek(0)
+    lines = referat_file.readlines()
     all_words = 0
     for index, value in enumerate(lines):
-        words = len(value.split())        
-        all_words += words
-print(all_words)
+      words = len(value.split())        
+      all_words += words 
+    print(all_words)  
+    
 
-
-referat_file = open('referat.txt', 'r') # Меняем точки в тексте на восклицательные знаки 
-new_referat_file = open( 'referat2.txt', 'w')
-
-for line in referat_file:
-  w= line.replace('.', '!')
-  new_referat_file.write(w)
-referat_file.close()
-new_referat_file.close()
